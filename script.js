199c0532cc916e4eb85259e7c34bf99f7b3c403a
l3r05a <200935142+L3r05a@users.noreply.github.com>
2025-08-22 16:33:57 +0100

    Add first operand logic

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-const display = document.querySelector("screen");
-
-const plus = document.querySelector("add");
-
-const minus = document.querySelector("sub");
-
-const mult = document.querySelector("multi");
-
-const divi = document.querySelector("divide");
-
-const equ = document.querySelector("equal");
-
-const reset = document.querySelector("clear");
-
-
 //Operators functions
-
 function addition (nA,nB){
     
     return nA + nB;
@@ -36,15 +20,44 @@ function division (nA,nB){
 }
 
 //Variables for operation and display
-let displayA,displayB,displayOp;
+let displayA='';
+let displayB='';
+let displayOp='';
 
-function operate (){
 
-    addition();
-    subtraction();
-    multiplication;
-    division();
+//Takes operator and numbers and feeds into Operattors function
+
+function operate (operator,numA,numB){
+
+    addition(numA,numB);
+    subtraction(numA,numB);
+    multiplication(numA,numB);
+    division(numA,numB);
 }
+
+
+//Selectors
+const display = document.querySelector('.screen');
+const numerals = document.querySelectorAll('.digit');
+
+
+//Numbers buttons to display and first operand variable
+numerals.forEach(button => {
+
+    button.addEventListener('click', function(){
+
+displayA += button.value;
+
+display.value = displayA;
+
+return displayA
+
+    })
+    
+});
+
+
+
 
 
 
